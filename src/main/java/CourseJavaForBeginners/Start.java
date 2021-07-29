3ed3c1f4da01f3154498fdd86de939e4f75d91ca
xenia-efimova <ks.v.efimova@gmail.com>
2021-07-30 02:01:37 +0300

    first homework added

---
+++ src/main/java/CourseJavaForBeginners/Start.java
@@ -0,0 +1,12 @@
+package CourseJavaForBeginners;
+
+public class Start {
+    public static void main (String[] args) {
+
+        double a = 15;
+        double b = 2;
+        double result = a / b;
+
+        System.out.println("Результат:" +  result);
+    }
+}
