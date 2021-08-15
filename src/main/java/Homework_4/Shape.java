6246ea616f5371fbd73ea16e36fce1d685ed983c
xenia-efimova <88164533+xenia-efimova@users.noreply.github.com>
2021-08-15 17:54:01 +0300

    Delete src/main/java/Homework_4 directory

--- src/main/java/Homework_4/Shape.java
+++
@@ -1,9 +0,0 @@
-package Homework_4;
-
-public abstract class Shape {
-    public abstract double getGirth();
-    public abstract double getArea();
-    public double getCircumference() {
-        return 0;
-    }
-}
